5b1e738423dc7083926d36b1b6e79c2e313cbf68
Seth Deegan <jayandseth@gmail.com>
2018-05-06 10:03:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+function request(username) {
+  var riot = new XMLHttpRequest();
+  riot.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      response = this.responseText;
+    }
+  };
+  riot.open('GET', 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + username + '?api_key=' + process.env.apikey, true);
+  riot.open();
+  return response
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content.indexOf('API') === 0) {
+    msg.reply(request(msg.content.slice(3)));
   }
 });
 
-client.login('token');
+client.login(process.env.token);
