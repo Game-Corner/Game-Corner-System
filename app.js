489302176c4ed7bdbe3b9daf85297e56734bc743
Seth Deegan <jayandseth@gmail.com>
2018-05-06 18:49:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -24,40 +24,40 @@ client.on('message', msg => {
         console.log(response);
         if (res.statusCode !== 200) {
           switch (res.statusCode) {
-            case: 400
+            case 400:
               msg.reply('Something went wrong with the request! Please try again.');\
               break;
-            case: 401
+            case 401:
               msg.reply('The developer of GC-System is not authorized to use the Riot API. Please contact them for furthur details.');
               break;
-            case: 403
+            case 403:
               msg.reply('The username ' + username + ' is not found in NA1');
               break;
-            case: 404
+            case 404:
               msg.reply('The username ' + username + ' is not found in NA1');
               break;
-            case: 405
+            case 405:
               msg.reply('The method ' + method + ' is not valid. Please go to <https://developer.riotgames.com/api-methods/#summoner-v3/GET_getBySummonerName> to see all availible methods.');
               break;
-            case: 415
+            case 415:
               msg.reply('The inputed text is not supported.');
               break;
-            case: 422
+            case 422:
               msg.reply(username+ ' exists, but hasn\'t played since match history collection began.');
               break;
-            case: 429
+            case 429:
               msg.reply('Too many requests are being made to the API. Please try again later.');
               break;
-            case: 500
+            case 500:
               msg.reply('There is an internal server error. Please contact the Riot Developer team here: <https://developer.riotgames.com/support/tickets/>.');
               break;
-            case: 502
+            case 502:
               msg.reply('There is a bad gateway. Please contact the developer of the bot.');
               break;
-            case: 503
+            case 503:
               msg.reply('The Riot API is currently unavailible. Please see <https://developer.riotgames.com/api-status/> for more details.');
               break;
-            case: 504
+            case 504:
               msg.reply('The response took too long. Please contact the developer of the bot.');
               break;
           }
