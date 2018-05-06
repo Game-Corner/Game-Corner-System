79d2516b08705928bb43aa15b982012d37fcdb83
Seth Deegan <jayandseth@gmail.com>
2018-05-06 14:59:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const http = require("http");
+const https = require("https");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,39 +9,17 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content.indexOf('API') === 0) {
     var username = msg.content.slice(3);
-    http.get('http://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + username + '?api_key=' + process.env.apikey, (res) => {
-      const { statusCode } = res;
-      const contentType = res.headers['content-type'];
+    https.get('http://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + username + '?api_key=' + process.env.apikey, (res) => {
+      console.log('statusCode:', res.statusCode);
+      console.log('headers:', res.headers);
 
-      let error;
-      if (statusCode !== 200) {
-        error = new Error('Request Failed.\n' +
-                          `Status Code: ${statusCode}`);
-      } else if (!/^application\/json/.test(contentType)) {
-        error = new Error('Invalid content-type.\n' +
-                          `Expected application/json but received ${contentType}`);
-      }
-      if (error) {
-        console.error(error.message);
-        // consume response data to free up memory
-        res.resume();
-        return;
-      }
-
-      res.setEncoding('utf8');
-      let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
-      res.on('end', () => {
-        try {
-          const parsedData = JSON.parse(rawData);
-          console.log(parsedData);
-        } catch (e) {
-          console.error(e.message);
-        }
+      res.on('data', (d) => {
+        process.stdout.write(d);
       });
+
     }).on('error', (e) => {
-      console.error(`Got error: ${e.message}`);
-    });
+      console.error(e);
+  });
   }
 });
 
