1142b03392028edad8a928cdbc5f9098ef9c5483
Seth Deegan <jayandseth@gmail.com>
2018-05-06 14:56:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,39 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content.indexOf('API') === 0) {
-    var username = msg.content.slice(3)
+    var username = msg.content.slice(3);
     http.get('http://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + username + '?api_key=' + process.env.apikey, (res) => {
-      msg.reply(res);
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
+
+      let error;
+      if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' +
+                          `Status Code: ${statusCode}`);
+      } else if (!/^application\/json/.test(contentType)) {
+        error = new Error('Invalid content-type.\n' +
+                          `Expected application/json but received ${contentType}`);
+      }
+      if (error) {
+        console.error(error.message);
+        // consume response data to free up memory
+        res.resume();
+        return;
+      }
+
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => { rawData += chunk; });
+      res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          console.log(parsedData);
+        } catch (e) {
+          console.error(e.message);
+        }
+      });
+    }).on('error', (e) => {
+      console.error(`Got error: ${e.message}`);
     });
   }
 });
