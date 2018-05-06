284eb2110ba8e9eb68d3942297896591fd0f9674
Seth Deegan <jayandseth@gmail.com>
2018-05-06 16:19:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ client.on('message', msg => {
   if (msg.content.indexOf('API.') === 0) {
     var content = msg.content;
     var reststring = content.slice(4);
-    var propertyVal = reststring.indexOf('.');
-    var username = reststring.slice(0, propertyVal);
-    var property = reststring.slice(propertyVal + 1);
+    var propertyPos = reststring.indexOf('.');
+    var username = reststring.slice(0, propertyPos);
+    var property = reststring.slice(propertyPos + 1);
     https.get('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + username + '?api_key=' + process.env.apikey, (res) => {
       console.log('statusCode:', res.statusCode);
       console.log('headers:', res.headers);
 
       res.on('data', (d) => {
         var response = JSON.parse(d);
-        console.log(response. + property);
-        msg.reply('The ' + property + ' of ' + username + ' is ' + 'yay');
+        console.log(response[property]);
+        msg.reply('The ' + property + ' of ' + username + ' is ' + response[property]);
       });
 
     }).on('error', (e) => {
