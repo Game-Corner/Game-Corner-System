2b1ff85e3459eea6bf9f3cc944c2c1bbd40d30f7
Seth Deegan <jayandseth@gmail.com>
2018-05-06 20:15:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const https = require("https");
+const port = process.env.PORT;
 
 const requestHandler = (request, response) => {
   console.log(request.url);
