c5c1647c51ccfc470b37470c7eb6dc1e3d159be1
Seth Deegan <jayandseth@gmail.com>
2018-05-06 19:47:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ client.on('message', msg => {
     var reststring = content.slice(4);
     var propertyPos = reststring.indexOf('.');
     if (propertyPos !== -1) {
-      username = reststring.slice(0, propertyPos);
-      method = reststring.slice(propertyPos + 1);
+      window.username = reststring.slice(0, propertyPos);
+      window.method = reststring.slice(propertyPos + 1);
     }
     if (username.match('^[A-z0-9 ]+$')) {
       https.get('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + username + '?api_key=' + process.env.apikey, (res) => {
