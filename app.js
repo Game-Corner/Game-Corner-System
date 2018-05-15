05938314b1b090c5d14c8e2689f2a3a13c327c23
Seth Deegan <jayandseth@gmail.com>
2018-05-15 09:41:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ server.listen(port, (err) => {
   console.log(`server is listening on ${port}`);
 });
 
-
+/*
 var rule = new schedule.RecurrenceRule();
 rule.hour = 9;
 rule.minute = 19;
@@ -65,10 +65,50 @@ var j = schedule.scheduleJob(rule, function() {
     });
   });
 });
+*/
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  j;
+  // Generate SMTP service account from ethereal.email
+  nodemailer.createTestAccount((err, account) => {
+    if (err) {
+        console.error('Failed to create a testing account. ' + err.message);
+        return process.exit(1);
+    }
+
+    console.log('Credentials obtained, sending message...');
+
+    // Create a SMTP transporter object
+    let transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+            user: account.user,
+            pass: account.pass
+        }
+    });
+
+    // Message object
+    let message = {
+        from: 'lectrician16@gmail.com',
+        to: process.env.email,
+        subject: 'Nodemailer is unicode friendly ✔',
+        text: 'Hello to myself!',
+        html: '<p><b>Hello</b> to myself!</p>'
+    };
+
+    transporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log('Error occurred. ' + err.message);
+            return process.exit(1);
+        }
+
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+  });
 });
 
 client.on('message', msg => {
