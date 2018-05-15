e00453219b530423f66e12976c8a569141793ddd
Seth Deegan <jayandseth@gmail.com>
2018-05-15 07:50:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,40 +26,41 @@ var rule = new schedule.RecurrenceRule();
 rule.hour = 17;
  
 var j = schedule.scheduleJob(rule, function() {
+  const nodemailer = require('nodemailer');
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
   nodemailer.createTestAccount((err, account) => {
-      // create reusable transporter object using the default SMTP transport
+    // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: account.smtp.port,
-      secure: account.smtp.secure,
-      auth: {
-        user: account.user, // generated ethereal user
-        pass: account.pass // generated ethereal password
-      }
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass // generated ethereal password
+        }
     });
 
     // setup email data with unicode symbols
     let mailOptions = {
-      from: '', // sender address
-      to: process.env.email, // list of receivers
-      subject: 'Hello ✔', // Subject line
-      text: 'Hello world?', // plain text body
-      html: '&lt;b&gt;Hello world?&lt;/b&gt;' // html body
+        from: '', // sender address
+        to: process.env.email, // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
     };
 
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) =&gt; {
-      if (error) {
-        return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);
-      // Preview only available when sending through an Ethereal account
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-          // Message sent: &lt;<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="3d5f0b08055b055e5c100b0f040b105e5e5b0910050e0d0b10050a59080a5c0d5f090e0f0c7d58455c504d5158135e5250">[email&#160;protected]</a>&gt;
-          // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
   });
 });
