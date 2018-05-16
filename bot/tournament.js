b9ac5aa16f63096427ecfd88c3429d053b0848ff
Seth Deegan <jayandseth@gmail.com>
2018-05-16 14:09:54 -0500

    Update tournament.js

--- bot/tournament.js
+++ bot/tournament.js
@@ -2,7 +2,7 @@
 To create a team: GC.gc.events("Annual League of Legends Summer Group Tournament").create.team(<teamName>, <public/private> (if private, prompt for a password in DMs));
 To invite someone: GC.gc.events("Annual League of Legends Summer Group Tournament").invite(<discord username>); Will send a 
 To register: GC.gc.events("Annual League of Legends Summer Group Tournament").register(<team>, <LoLUsername>, role);
-To view tournament: GC.gc.events("Annual League of Legends Summer Group Tournament").view(<teams/info/description/rules>);
+To view tournament: GC.gc.events("Annual League of Legends Summer Group Tournament").view(<teams/info/description/rules/results>);
 
 remember if an array value has parentheses, then reject rest of loop and output msgsplit position of part that has parenthases
 
@@ -11,4 +11,8 @@ remember if an array value has parentheses, then reject rest of loop and output 
 function() {
   
   var msgsplit[res.msglength + 1]
+  
+  
+  // Create a team
+  
 }
