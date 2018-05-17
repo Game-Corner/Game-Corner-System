4667255e551156858d8484a98bb102ac07904273
Seth Deegan <jayandseth@gmail.com>
2018-05-17 10:52:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const https = require("https");
+const http = require("http");
 const port = process.env.PORT;
 
 // Server keeps the bot with Uptime Robot pinging it
@@ -9,7 +9,7 @@ const requestHandler = (request, response) => {
   response.end('Hello, I am Geekybot!');
 }
 
-const server = https.createServer(requestHandler)
+const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
   if (err) {
