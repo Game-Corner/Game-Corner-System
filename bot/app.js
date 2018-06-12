91197f90e2b74f0b70ef68eec59f05a951f69c16
Seth Deegan <jayandseth@gmail.com>
2018-06-12 12:24:09 -0500

    Update app.js

--- bot/app.js
+++ bot/app.js
@@ -39,17 +39,20 @@ client.on('ready', () => {
 client.on('guildMemberRemove', member => {
   member.createDM()
     .then(DMchannel => {
-      DMchannel.send('Hey there, we\'d like to know why you left Game Corner so that future members have a better experience. Please type out your response in a message below. Thanks!');
-      console.log('1 received');
-      const filter = m => m.content.length >= 1;
-      DMchannel.awaitMessages(filter, { max: 1, time: 86400000, errors: ['time'] })
-        .then(collected => {
-          console.log('2 received');
-          client.fetchUser('240550416129982464').then(user => {
-            console.log('3 received');
-            console.log(collected.values());
-            user.send(collected.values().next().value);
-          });
+      DMchannel.send('Hey there, we\'d like to know why you left Game Corner so that future members have a better experience. Please type out your response in a message below. Thanks!')
+        .then(() => {
+          console.log('1 received');
+          const filter = m => m.content.length >= 1;
+          DMchannel.awaitMessages(filter, { max: 1, time: 86400000, errors: ['time'] })
+            .then(collected => {
+              console.log('2 received');
+              client.fetchUser('240550416129982464')
+                .then(user => {
+                  console.log('3 received');
+                  console.log(collected.values());
+                  user.send(collected.values().next().value);
+                });
+            });
         });
     });
 });
